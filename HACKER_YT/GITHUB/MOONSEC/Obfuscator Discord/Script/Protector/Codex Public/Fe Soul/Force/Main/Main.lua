69fceaa9ddb878b34f7be29d9084b7df713fc450
XDMODZ1040 <98317096+XDMODZ1040@users.noreply.github.com>
2022-06-10 20:40:52 +0800

    ToastFeScript

--- HACKER_YT/GITHUB/MOONSEC/Obfuscator Discord/Script/Protector/Codex Public/Fe Soul/Force/Main/Main.lua
+++ HACKER_YT/GITHUB/MOONSEC/Obfuscator Discord/Script/Protector/Codex Public/Fe Soul/Force/Main/Main.lua
@@ -1,6 +1,6 @@
 
----convert By Im Patrick
----using Myworld Reinimate
+---CONVERT SCRIPT BY HACKER_YT
+---SCRIPT GITHUB BASE
 
 for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then game:GetService("RunService").Heartbeat:connect(function() v.Velocity = Vector3.new(-30,0,0) end) end end game:GetService("StarterGui"):SetCore("SendNotification", { Title = "CREDIT"; Text = "MADE BY HACKER_YT"; Icon = ""}) Duration = 16;
 
